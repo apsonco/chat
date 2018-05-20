dfcf0de8d052948683b51d5b4b67cac5ea95872b
pavel.uh@gmail.com <pavel.uh@gmail.com>
2018-05-20 17:10:16 +0300

    Resolve merging errors

--- test_utils.py
+++ test_utils.py
@@ -1,5 +1,4 @@
 # test_utils.py
-<<<<<<< HEAD
 # Pytests for utils.py
 
 import json
@@ -30,12 +29,3 @@ class TestCoding:
         decoded_message = result_message.decode(config.CHARACTER_ENCODING)
         test_message = json.loads(decoded_message)
         assert origin_json == test_message
-=======
-#
-# Python tests for utils.py
-
-import utils
-
-
-test_message_json = utils.dict_to_bytes()
->>>>>>> master
